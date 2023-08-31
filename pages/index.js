46e0f8e7021c5af39ca2069dfd1107beeb085df8
Victor Moraes <victor.moraesrj@gmail.com>
2023-08-31 16:58:43 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Home page</h1>
+        <a href='/teste'>Página de teste, cai em um 404</a> 
+    </div>
+  )
+}
+
+export default HomePage
